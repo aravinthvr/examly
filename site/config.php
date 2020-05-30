179cb6b97383cbe1488d2dd71efc3480906fbf0a
aravinthvr <aravinthaprasath.1706005@srec.ac.in>
2020-05-30 18:50:44 +0530

    EXAMLY INAPP NOTIFICATION.

--- site/config.php
+++ site/config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'notify');
+   define('DB_SERVER', 'db4free.net');
+   define('DB_USERNAME', 'aravinth');
+   define('DB_PASSWORD', 'aravinthvr');
+   define('DB_DATABASE', 'notification');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
