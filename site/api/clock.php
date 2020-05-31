9c56c1593b92cc0a13fa99393a2638c39a1bb2e4
aravinthvr <aravinthaprasath.1706005@srec.ac.in>
2020-05-31 19:06:37 +0530

    changes

--- site/api/clock.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-echo "Time: ".date("h:i:s");
-echo '<div>';
-echo "Date: ".date("Y-m-d");
-echo '</div>';
-?>
--- site/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'db4free.net');
-   define('DB_USERNAME', 'aravinth');
-   define('DB_PASSWORD', 'aravinthvr');
-   define('DB_DATABASE', 'notification');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
